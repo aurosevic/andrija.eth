6702f81bcef1e34cd18db8fb4c3e16ae0955a9ed
Andrija Urosevic <andrija.urosevic27@gmail.com>
2025-05-17 20:29:55 +0200

    Added padding between Icon and its Label.

--- src/components/IconLink/IconLink.js
+++ src/components/IconLink/IconLink.js
@@ -4,15 +4,14 @@ import './IconLink.css';
 const IconLink = ({ href, IconComponent = null, iconSrc = null, alt, label, width = "50", height="50" }) => {
   return (
     <div className="main-text icon-link">
-      <a href={href} target="_blank" rel="noopener noreferrer">
-        {IconComponent ? (
-          <IconComponent width={width} height={height} />
-        ) : (
-          <img src={iconSrc} alt={alt} width={width} height={height} />
-        )}
-        <br />
-        <span>{label}</span>
-      </a>
+        <a href={href} target="_blank" rel="noopener noreferrer">
+            {IconComponent ? (
+                <IconComponent width={width} height={height}/>
+            ) : (
+                <img src={iconSrc} alt={alt} width={width} height={height}/>
+            )}
+            <span style={{paddingTop: '14px'}}>{label}</span>
+        </a>
     </div>
   );
 };
